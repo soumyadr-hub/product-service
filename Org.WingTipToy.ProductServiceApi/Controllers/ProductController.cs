a28b1db2cb7acb968fd240929b5536a361e3e6a4
droy.soumya.contact@gmail.com <droy.soumya.contact@gmail.com>
2020-08-11 19:59:38 -0400

    Basic Sloution Structure

---
+++ Org.WingTipToy.ProductServiceApi/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Org.WingTipToy.ProductServiceApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/api")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("HealthCheck")]
+        public IActionResult Get()
+        {
+            return Ok("All Good");
+        }
+    }
+}
